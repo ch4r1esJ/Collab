ef54019e0cb462ab101a7c73ba0c601593869ad8
ch4r1esJ <Charles.Janjgava.Git@gmail.com>
2025-12-20 15:47:30 +0400

    First Commit

---
+++ Collaboration/Collaboration/CollaborationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollaborationApp.swift
+//  Collaboration
+//
+//  Created by Charles Janjgava on 12/20/25.
+//
+
+import SwiftUI
+
+@main
+struct CollaborationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
